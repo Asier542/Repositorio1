d77d5465a20fd5c881320683fe3201db8041ae83
Asier542 <152624117+Asier542@users.noreply.github.com>
2024-02-09 16:14:34 +0100

    Update JavaApplication10.java

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication10;
-
+import java.utils.Scanner;
 /**
  *
  * @author DAM105
@@ -14,7 +14,10 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    Scanner sc=new Scanner(System.in);
+    system.out.println("dime un numero")
+    int numero;
+    system.out.println("Tu numero es "+ numero);
     }
     
 }
